d8d312951d41e3ed8f7ee0fac4b5d35160a24d61
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-07-25 15:36:34 -0300

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -18,29 +18,19 @@ node3 = new node(100);
 
 
 //later i will add it in class node
-function add (node) {
+function add (n) {
     if (LinkedList.head === null){
-    LinkedList.head = node.value;
-    LinkedList.tail = node.value;
+    LinkedList.head = n;
+    LinkedList.tail = n;
 } else {
-   LinkedList.tail = node1;
-   node.next = node.value;
+   LinkedList.tail.next = n;
+   LinkedList.tail = n;
 }
 }
 
-
-list = [node1, node2, node3];
 
 add(node1);
 add(node2);
 add(node3);
 
-//print the Linked List
-for(var i = 0; i < list.length; i++){
-    if (i===0){
-    console.log(LinkedList.head)
-    } else{
-    console.log(list[i].next)
-    }
-}
-
+console.log(LinkedList.head)
