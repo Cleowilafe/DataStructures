311d2d4551129334c3200abc2c3e38e066768f01
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-07-30 12:58:58 -0300

    Update Binarytree.js

--- Binarytree.js
+++ Binarytree.js
@@ -4,27 +4,49 @@ class node {
        this.left = null;
        this.right = null;
    }
-  
-    
-} // end node 
 
 
+} // end node
  class BinaryTree{
      constructor (){
          this.root = null;
-         
+
      }
- 
+   insertnode(current, node){
+     if (node.value < current.value){
+       if(current.left === null){
+         current.left = current
+       } else {
+         this.insertnode(current.left, node);
+       } // end if
+     } // end if daddy
+
+     if (node.value > current.value){
+       if(current.right === null){
+         current.right = current
+       } else {
+         this.insertnode(current.right, node);
+       } // end if
+     } // end if daddy
+
+   }
+
    insert (node){
        if (this.root === null){
            this.root = node;
-       }
-   } 
-} 
+       } else {
+         this.insertnode(this.root, node);
+       } // end if
+   } // end insert
+}
 
 BinaryTree = new BinaryTree ();
-node1 = new node (12);
+node1 = new node (23);
+node2 = new node (14);
+node3 = new node (31);
 
 BinaryTree.insert(node1);
+BinaryTree.insert(node2);
+BinaryTree.insert(node3);
 
-console.log(BinaryTree.root);
+//add traverse method 
